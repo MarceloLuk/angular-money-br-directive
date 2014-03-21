d5ac64e2074ef622811afbb6b27ed7907a1dcf2b
Marcelo Gomes de Oliveira <marcelo.oliveira@xys.com.br>
2014-03-21 16:01:56 -0300

    [add] adicionando diretiva

---
+++ angular-money-br-directive.js
@@ -0,0 +1,52 @@
+﻿/**
+ * Diretiva para adicionar máscara da moeda brasileira (R$)
+ *
+ * O Parametro zero-filter="true" e para limpar os dados ao inserir o valor 0 ex: 0,00
+ * EX: <input  format="number" zero-filter="true" />
+ * @author = "Marceloluk"
+ * GitHub: https://github.com/MarceloLuk/angular-money-br-directive.git
+ */
+angular.module('luk.money', [])
+    .directive('format', ['$filter', function ($filter) {
+    return {
+        require: '?ngModel',
+        link: function (scope, elem, attrs, ctrl) {
+            if (!ctrl) return;
+
+            ctrl.$formatters.unshift(function (a) {
+                return $filter(attrs.format)(ctrl.$modelValue)
+            });
+
+            ctrl.$parsers.unshift(function (viewValue) {
+                if (viewValue.length <= 3) {
+                    viewValue = '00'+viewValue;
+                }
+                var value = viewValue;
+                value = value.replace(/\D/g,"");
+                value = value.replace(/(\d{2})$/,",$1");
+                value = value.replace(/(\d+)(\d{3},\d{2})$/g,"$1.$2");
+                var qtdLoop = (value.length-3)/3;
+                var count = 0;
+                while (qtdLoop > count)
+                {
+                    count++;
+                    value = value.replace(/(\d+)(\d{3}.*)/,"$1.$2");
+                }
+                var plainNumber = value.replace(/^(0)(\d)/g,"$2");
+
+                elem.val(plainNumber);
+                return plainNumber;
+            });
+
+            elem.bind('blur', function () {
+                var valueFilter = elem.val();
+                valueFilter = valueFilter.replace(/\D/g,"");
+                if (attrs.zeroFilter == 'true') {
+                    if (valueFilter == 0) {
+                        elem.val('');
+                    }
+                }
+            });
+        }
+    };
+}]);
